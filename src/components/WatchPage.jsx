35277d0b0e24c4f9a9a9af54eeeb8f5bb02e369b
Ajinkya243 <ajinkyagund2@gmail.com>
2025-07-10 12:55:22 +0530

    feat:add comment section

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer';
+import LiveChat from './LiveChat';
 
 const WatchPage = () => {
     const dispatch=useDispatch();
@@ -11,8 +13,16 @@ const WatchPage = () => {
         dispatch(closeMenu());
     },[])
   return (
-    <div className='px-5'>
-      <iframe width="1200" height="600" src={`https://www.youtube.com/embed/${id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <div className='w-full'>
+    <div className='px-5 flex w-full'>
+      <div>
+      <iframe width="1100" height="600" src={`https://www.youtube.com/embed/${id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+      <div className='w-full'>
+        <LiveChat/>
+      </div>
+    </div>
+    <CommentsContainer/>
     </div>
   )
 }
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -2,4 +2,4 @@ const GOOGLE_API_KEY="AIzaSyDgKDn5_mKsqRmBf11z2K5sioQCmvSd2fA"
 
 export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
+export const YOUTUBE_SEARCH_API=`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=${GOOGLE_API_KEY}&q=`
